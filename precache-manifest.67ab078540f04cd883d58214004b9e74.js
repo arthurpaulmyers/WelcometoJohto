3ad89a0ca6515e196102f55fff725e8fc8e86ac7
arthurpaulmyers <54543515+arthurpaulmyers@users.noreply.github.com>
2019-10-30 14:39:20 -0400

    Updates

---
+++ precache-manifest.67ab078540f04cd883d58214004b9e74.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2268b02f0308dd93279f6f394778da5",
+    "url": "/WelcometoJohto/index.html"
+  },
+  {
+    "revision": "1ee910ab7026cbc298d5",
+    "url": "/WelcometoJohto/static/css/2.510409cd.chunk.css"
+  },
+  {
+    "revision": "61383ff2c7044f052181",
+    "url": "/WelcometoJohto/static/css/main.b43a58c0.chunk.css"
+  },
+  {
+    "revision": "1ee910ab7026cbc298d5",
+    "url": "/WelcometoJohto/static/js/2.f8615944.chunk.js"
+  },
+  {
+    "revision": "61383ff2c7044f052181",
+    "url": "/WelcometoJohto/static/js/main.9cbfa8c5.chunk.js"
+  },
+  {
+    "revision": "59e711f534be9d4af97f",
+    "url": "/WelcometoJohto/static/js/runtime-main.e6a9411b.js"
+  },
+  {
+    "revision": "650ed4e08eabc2e1233ef62931c7d630",
+    "url": "/WelcometoJohto/static/media/BellsproutDayImg.650ed4e0.png"
+  },
+  {
+    "revision": "bf892b9c76d061adf6dce9724807a5a6",
+    "url": "/WelcometoJohto/static/media/BugCatchingContestDayImg.bf892b9c.png"
+  },
+  {
+    "revision": "084ba22cd70caad848e168d0379f46a3",
+    "url": "/WelcometoJohto/static/media/DittoDayImg.084ba22c.png"
+  },
+  {
+    "revision": "2c794da2dc2c6c6ceff7605769ef6360",
+    "url": "/WelcometoJohto/static/media/Elm.2c794da2.jpg"
+  },
+  {
+    "revision": "f3f0f0d66c078f372701849fa4736079",
+    "url": "/WelcometoJohto/static/media/GoldenrodCityDayImg.f3f0f0d6.png"
+  },
+  {
+    "revision": "a49d87421241e19103c1307493395e5e",
+    "url": "/WelcometoJohto/static/media/HorseaDayImg.a49d8742.png"
+  },
+  {
+    "revision": "342c2e0ac377849e1bf5f2a40cd5ec28",
+    "url": "/WelcometoJohto/static/media/KimonoDanceDayImg.342c2e0a.png"
+  },
+  {
+    "revision": "831e827531c6d563896ba9d79a83a04f",
+    "url": "/WelcometoJohto/static/media/KyotoCityDayImg.831e8275.png"
+  },
+  {
+    "revision": "0faa69234c70b6b6e5ba8cd44044e6fb",
+    "url": "/WelcometoJohto/static/media/LegendaryBeastsDayImg.0faa6923.png"
+  },
+  {
+    "revision": "2bc2aaf7f9ae5eb5c65c8cad40470845",
+    "url": "/WelcometoJohto/static/media/MtSilverDayImg.2bc2aaf7.png"
+  },
+  {
+    "revision": "63336143099baad286b4df22d6411aea",
+    "url": "/WelcometoJohto/static/media/NaraCityDayImg.63336143.png"
+  },
+  {
+    "revision": "9b003895ffebd92da03c131eb4822444",
+    "url": "/WelcometoJohto/static/media/NarutoStraitDayImg.9b003895.png"
+  },
+  {
+    "revision": "10bd035fecec6b7b9d194f1d85de615a",
+    "url": "/WelcometoJohto/static/media/NewBarkTownDayImg.10bd035f.png"
+  },
+  {
+    "revision": "5788445f58df35af22f81e4e0c7e89e2",
+    "url": "/WelcometoJohto/static/media/OsakaCityDayImg.5788445f.png"
+  },
+  {
+    "revision": "b8420aa950efd43d5edcdf16390e420d",
+    "url": "/WelcometoJohto/static/media/RedBattleDayImg.b8420aa9.png"
+  },
+  {
+    "revision": "9e30855f2307a87bdf0aa13229367438",
+    "url": "/WelcometoJohto/static/media/SentretDayImg.9e30855f.png"
+  },
+  {
+    "revision": "ba55472e2d1cddc804784493b66568bc",
+    "url": "/WelcometoJohto/static/media/ShizuokaCityDayImg.ba55472e.png"
+  },
+  {
+    "revision": "9357e2aa09a55cb434d408497ade92cb",
+    "url": "/WelcometoJohto/static/media/StantlerDayImg.9357e2aa.png"
+  },
+  {
+    "revision": "ccdc40bd3c0380f5ad0f5c2870d2df11",
+    "url": "/WelcometoJohto/static/media/StarterDayImg.ccdc40bd.png"
+  },
+  {
+    "revision": "21810cb96b306b9e8a51e050e6ad8320",
+    "url": "/WelcometoJohto/static/media/TyranitarDayImg.21810cb9.png"
+  },
+  {
+    "revision": "fc275f8daf09b64448de43ff9494237d",
+    "url": "/WelcometoJohto/static/media/WhirlIslandsDayImg.fc275f8d.png"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/WelcometoJohto/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "f67f6ab5f27ebda05fc1eab229d30861",
+    "url": "/WelcometoJohto/static/media/poke-ball.f67f6ab5.png"
+  }
+]);
